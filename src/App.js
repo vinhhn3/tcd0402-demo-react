155ae9c1af5fd18d5150675a5a8336496d85445a
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-06-08 10:10:05 +0700

    Hello World React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const clickMe = () => {
+    setCounter(counter + 1);
+  };
+  const [isDisplay, setDisplay] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isDisplay && <p>Hello World</p>}
+        <p>{counter}</p>
+        <button onClick={clickMe}>Click me</button>
       </header>
     </div>
   );
